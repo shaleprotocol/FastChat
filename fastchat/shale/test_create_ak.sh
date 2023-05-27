e154bb4bb0db27a56b83f7cadd187d44816dfdff
Ning Ren <renning22@gmail.com>
2023-05-27 23:00:17 +0000

    use new domain

--- fastchat/shale/test_create_ak.sh
+++ fastchat/shale/test_create_ak.sh
@@ -1,17 +1,17 @@
 
-curl http://api.shaleprotocol.com:30085/v1/shale_create_api_key \
+curl http://dev.shale.live:30085/v1/shale_create_api_key \
   -H "Content-Type: application/json" \
   -d '{
     "secret": "'$1'"
   }'
 
-curl http://api.shaleprotocol.com:30085/v1/shale_create_api_key \
+curl http://dev.shale.live:30085/v1/shale_create_api_key \
   -H "Content-Type: application/json" \
   -d '{
     "secret": "'$1'", "user_id": "test_user"
   }'
 
-curl http://api.shaleprotocol.com:30085/v1/shale_create_api_key \
+curl http://dev.shale.live:30085/v1/shale_create_api_key \
   -H "Content-Type: application/json" \
   -d '{
     "secret": "'$1'", "user_id": "test_user", "user_email": "test@shaleprotocol.com"
