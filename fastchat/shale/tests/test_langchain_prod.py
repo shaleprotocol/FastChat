234c1f60931a6afc3645b4e787ea60a02c013cb2
Ning Ren <renning22@gmail.com>
2023-05-28 14:07:44 -0700

    test_langchain_prod.py

--- fastchat/shale/tests/test_langchain_prod.py
+++ fastchat/shale/tests/test_langchain_prod.py
@@ -1,11 +1,12 @@
 import os
-os.environ['OPENAI_API_BASE'] = "http://dev.shale.live:30085/v1"
+
+os.environ['OPENAI_API_BASE'] = "https://shale.live/v1"
 os.environ['OPENAI_API_KEY'] = "shale-IcLnUMpmnIoVeJ"
 
 from langchain.llms import OpenAI
 
 llm = OpenAI(temperature=0.9)
-prompt = "Write a poem about python and ai"
+prompt = "Write me a python that parse json and sort first entry."
 for i in range(2):
     result = llm(prompt)
     print(result)
